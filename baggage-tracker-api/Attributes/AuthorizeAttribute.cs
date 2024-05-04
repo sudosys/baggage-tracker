09a71f839c35cc1fc99139febf3fe052e3d77a06
sudosys <ergin.ahmedtaha@gmail.com>
2024-05-04 19:03:23 +0300

    feat(backend): add skip auth check

--- baggage-tracker-api/Attributes/AuthorizeAttribute.cs
+++ baggage-tracker-api/Attributes/AuthorizeAttribute.cs
@@ -1,29 +1,38 @@
-using BaggageTrackerApi.Entities.DTOs;
+using BaggageTrackerApi.Entities;
 using BaggageTrackerApi.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BaggageTrackerApi.Attributes;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class AuthorizeAttribute(UserRole? role) : Attribute, IAuthorizationFilter
+public class AuthorizeAttribute(bool personnelOnly = false) : Attribute, IAuthorizationFilter
 {
     private readonly JsonResult _unauthenticatedResult =
         new(new { Message = "Authentication needed to perform this action.", StatusCode = StatusCodes.Status401Unauthorized });
     
     private readonly JsonResult _forbiddenResult =
-        new(new { Message = $"Only users with {role.ToString()} role are authorized to perform this action.", StatusCode = StatusCodes.Status403Forbidden});
+        new(new { Message = $"Only {nameof(UserRole.Personnel)} is authorized to perform this action.", StatusCode = StatusCodes.Status403Forbidden});
     
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        var user = (UserDto?)context.HttpContext.Items["User"];
+        if (SkipAuthorization(context))
+        {
+            return;
+        }
+        
+        var user = (User?)context.HttpContext.Items["User"];
 
         if (user == null)
         {
             context.Result = _unauthenticatedResult;
-        } else if (role != null && user.Role != role)
+        } else if (personnelOnly && user.Role != UserRole.Personnel)
         {
             context.Result = _forbiddenResult;
         }
     }
+
+    private static bool SkipAuthorization(AuthorizationFilterContext context) => 
+        context.ActionDescriptor.EndpointMetadata.Any(e => e.GetType() == typeof(AllowAnonymousAttribute));
 }
