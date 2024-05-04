40e830bf1a9bb20d439ab0a0dd6cad0b942f43c2
sudosys <ergin.ahmedtaha@gmail.com>
2024-05-04 15:56:22 +0300

    refactor(backend): remove mock data related classes, perform data seeding instead

--- baggage-tracker-api/BaggageTrackerDbContext.cs
+++ baggage-tracker-api/BaggageTrackerDbContext.cs
@@ -1,19 +1,25 @@
 using BaggageTrackerApi.Entities;
+using BaggageTrackerApi.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace BaggageTrackerApi;
 
-public class BaggageTrackerDbContext : DbContext
+public class BaggageTrackerDbContext(DbContextOptions<BaggageTrackerDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Flight> Flights { get; set; }
     public DbSet<Baggage> Baggages { get; set; }
 
-    public BaggageTrackerDbContext(DbContextOptions<BaggageTrackerDbContext> options) : base(options)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        ConstructRelations(modelBuilder);
+
+        AddSeedData(modelBuilder);
+
+        base.OnModelCreating(modelBuilder);
     }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    private static void ConstructRelations(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
             .HasOne(u => u.ActiveFlight)
@@ -24,7 +30,32 @@ public class BaggageTrackerDbContext : DbContext
             .HasMany(u => u.Baggages)
             .WithOne(b => b.User)
             .HasForeignKey(b => b.UserId);
+    }
 
-        base.OnModelCreating(modelBuilder);
+    private static void AddSeedData(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(new List<User>
+        {
+            new(UserRole.Passenger, "avery.thompson", "Avery", "Thompson"),
+            new(UserRole.Passenger, "sebastian.morales", "Sebastian", "Morales"),
+            new(UserRole.Passenger, "olivia.martinez", "Olivia", "Martinez"),
+        });
+        
+        modelBuilder.Entity<Flight>().HasData(new List<Flight>
+        {            
+            new("TK5094", 1),
+            new("TK5094", 2),
+            new("TK2745", 3),
+        });
+        
+        modelBuilder.Entity<Baggage>().HasData(new List<Baggage>
+        {
+            new("T436712", 1, BaggageStatus.Undefined),
+            new("T377053", 1, BaggageStatus.Undefined),
+            new("T205967", 1, BaggageStatus.Undefined),
+            new("T519736", 2, BaggageStatus.Undefined),
+            new("T724821", 3, BaggageStatus.Undefined),
+            new("T541263", 3, BaggageStatus.Undefined),
+        });
     }
 }
