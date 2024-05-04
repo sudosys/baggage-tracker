b1a8dd7652410f19b9fd7e91156cb6cf39e4a483
sudosys <ergin.ahmedtaha@gmail.com>
2024-05-04 19:04:20 +0300

    feat(backend): add open api info & security scheme

--- baggage-tracker-api/Program.cs
+++ baggage-tracker-api/Program.cs
@@ -2,6 +2,7 @@ using BaggageTrackerApi.Middlewares;
 using BaggageTrackerApi.Models;
 using BaggageTrackerApi.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace BaggageTrackerApi;
 
@@ -16,7 +17,38 @@ public class Program
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(swagger =>
+        {
+            swagger.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Baggage Tracker API",
+                Description = "RESTful API for Baggage Tracking application."
+            });
+            
+            swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                In = ParameterLocation.Header,
+                Description = "JWT authorization header using the Bearer scheme. e.g. Bearer [space] JWT_TOKEN_HERE"
+            });
+            swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
         builder.Services.AddControllers();
 
         ConfigureServices(builder);
@@ -57,5 +89,6 @@ public class Program
     private static void RegisterServices(IServiceCollection services)
     {
         services.AddScoped<UserService>();
+        services.AddScoped<AuthenticationService>();
     }
 }
