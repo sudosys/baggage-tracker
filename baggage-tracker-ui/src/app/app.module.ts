7eb96741b5bca4f3c0a0126e1ebb68ad098ac68f
sudosys <ergin.ahmedtaha@gmail.com>
2024-05-12 15:12:07 +0300

    feat(ui): create header and login components, configure routing for login

--- baggage-tracker-ui/src/app/app.module.ts
+++ baggage-tracker-ui/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent, LoginComponent, HeaderComponent],
+	imports: [BrowserModule, AppRoutingModule, InputTextModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
